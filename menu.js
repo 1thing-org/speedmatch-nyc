e3679e52e5133d4e2c23bdfc1ddf844b7ec474e8
Sylvie-Wxr <sylvie_wu125@outlook.com>
2025-07-17 13:36:46 -0700

    chore: remove legacy code

--- menu.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navOverlay = document.getElementById('nav-overlay');
-
-hamburger.addEventListener('click', () => {
-  navOverlay.classList.toggle('open');
-});
