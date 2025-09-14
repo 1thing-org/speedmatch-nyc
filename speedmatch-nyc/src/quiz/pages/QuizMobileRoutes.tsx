54fd86ed54997266528bb4ed775ac41396382b38
Li <lima01@gmail.com>
2025-09-14 12:43:21 -0400

    Remove unused Navigate

--- speedmatch-nyc/src/quiz/pages/QuizMobileRoutes.tsx
+++ speedmatch-nyc/src/quiz/pages/QuizMobileRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import QuizStart from "./mobile/QuizStart";
 import QuizQuestions from "./mobile/QuizQuestions";
 import QuizPick from "./mobile/QuizPick";
